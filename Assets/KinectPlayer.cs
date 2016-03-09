08ea00d334a7ea6c1a1867ea15e790d6864eefc7
21311047 <21311047yu@tama.ac.jp>
2016-03-09 20:57:53 +0900

    Tracking nearPlayer

--- Assets/KinectPlayer.cs
+++ Assets/KinectPlayer.cs
@@ -8,6 +8,7 @@ public class KinectPlayer : MonoBehaviour {
 	private Kinect.Body[] player=null;
 	private bool isHandTrack=false;
 	private GameObject right,left;
+	private int playerNum=-1;
 	// Use this for initialization
 	void Start () {
 		data = GameObject.Find ("BodySourceManager").GetComponent<BodySourceManager> ();
@@ -21,18 +22,31 @@ public class KinectPlayer : MonoBehaviour {
 		player=data.GetData();
 		if (player == null)
 			return;
-		foreach(Kinect.Body body in player)
-		{
-			if(body==null)
+		for (int i=0; i<player.Length; i++) {	
+			Kinect.Body body = player [i];
+			if (body == null)
 				return;
-			if(body.IsTracked)
-			{
-				Kinect.Joint LeftHand=body.Joints[Kinect.JointType.HandLeft];
-				Kinect.Joint RightHand=body.Joints[Kinect.JointType.HandRight];
-				left.transform.position=GetVector3FromJoint(LeftHand);
-				right.transform.position=GetVector3FromJoint(RightHand);
-				Debug.Log("Left:"+GetVector3FromJoint(LeftHand)+"\n"+"Right:"+GetVector3FromJoint(RightHand));
+			//一番近い人のトラッキングにするルール考える
+			float PlayerPos = -1000f;
+			if (body.IsTracked) {
+				Kinect.Joint Head = body.Joints [Kinect.JointType.Head];
+				Vector3 HeadPos = GetVector3FromJoint (Head);
+				if (PlayerPos < HeadPos.z)
+				{
+					playerNum=i;
+					PlayerPos = HeadPos.z;
+				}
+				else if(PlayerPos==-1000f)
+				{
+					playerNum=-1;
+				}
 			}
+		}
+		if (playerNum != -1) {
+			Kinect.Joint LeftHand = player[playerNum].Joints [Kinect.JointType.HandLeft];
+			Kinect.Joint RightHand = player[playerNum].Joints [Kinect.JointType.HandRight];
+			left.transform.position = GetVector3FromJoint (LeftHand);
+			right.transform.position = GetVector3FromJoint (RightHand);
 		}
 	}
 	private static Vector3 GetVector3FromJoint(Kinect.Joint joint)
