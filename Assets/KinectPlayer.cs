067ce08a4f286b39be09ba62835b245d042e3f5d
21311047 <21311047yu@tama.ac.jp>
2016-03-09 19:50:26 +0900

    add player

---
+++ Assets/KinectPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+public class KinectPlayer : MonoBehaviour {
+
+	BodySourceManager body;
+	private Body[] player=null;
+	// Use this for initialization
+	void Start () {
+		body = GameObject.Find ("BodySourceManager").GetComponent<BodySourceManager> ();
+	}
+	// Update is called once per frame
+	void Update () {
+		player=body.GetData ();
+
+	}
+}
