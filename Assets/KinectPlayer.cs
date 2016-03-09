c30023f5edf8a3dc50d7935c04562763597a891c
21311047 <21311047yu@tama.ac.jp>
2016-03-09 20:39:45 +0900

    move handObj

--- Assets/KinectPlayer.cs
+++ Assets/KinectPlayer.cs
@@ -7,9 +7,12 @@ public class KinectPlayer : MonoBehaviour {
 	BodySourceManager data;
 	private Kinect.Body[] player=null;
 	private bool isHandTrack=false;
+	private GameObject right,left;
 	// Use this for initialization
 	void Start () {
 		data = GameObject.Find ("BodySourceManager").GetComponent<BodySourceManager> ();
+		right = GameObject.Find ("Right");
+		left = GameObject.Find ("Left");
 	}
 	// Update is called once per frame
 
@@ -26,14 +29,14 @@ public class KinectPlayer : MonoBehaviour {
 			{
 				Kinect.Joint LeftHand=body.Joints[Kinect.JointType.HandLeft];
 				Kinect.Joint RightHand=body.Joints[Kinect.JointType.HandRight];
-				Debug.Log(GetVector3FromJoint(LeftHand));
-				Debug.Log(GetVector3FromJoint(RightHand));
-
+				left.transform.position=GetVector3FromJoint(LeftHand);
+				right.transform.position=GetVector3FromJoint(RightHand);
+				Debug.Log("Left:"+GetVector3FromJoint(LeftHand)+"\n"+"Right:"+GetVector3FromJoint(RightHand));
 			}
 		}
 	}
 	private static Vector3 GetVector3FromJoint(Kinect.Joint joint)
 	{
-		return new Vector3(joint.Position.X * 10, joint.Position.Y * 10, joint.Position.Z * 10);
+		return new Vector3(joint.Position.X * 10, joint.Position.Y * 10 +5, -joint.Position.Z * 10);
 	}
 }
