a235e1ea4f52cad1e8ad57b4327c2e0505a3ef83
232fumiya <21311232fn@tama.ac.jp>
2016-03-11 19:59:05 +0900

    add SettingWindow

---
+++ Assets/Scripts/windowSetting.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class windowSetting : MonoBehaviour {
+	Rect newWindow;
+	string a;
+	// Use this for initialization
+	void Start () {
+		newWindow = new Rect (0,0,100,100);
+	}
+	void OnGUI(){
+		newWindow=GUI.Window(1,newWindow,WindowFunc,"Setting");
+	}
+	void WindowFunc(int windowID){
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
