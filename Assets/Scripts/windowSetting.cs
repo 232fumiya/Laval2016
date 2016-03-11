e368715417455c1b4cd1617f2c5934e0b62d0f93
Fumiya <21311232fn@tama.ac.jp>
2016-03-12 00:16:48 +0900

    Update windowSetting.cs

--- Assets/Scripts/windowSetting.cs
+++ Assets/Scripts/windowSetting.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class windowSetting : MonoBehaviour {
 	private Rect newWindow;
  	private float Timer=60f;
+ 	private bool shortMode=false;
+	private bool midleMode=true;
+	private bool longMode=false;
 	private GameController GameControllerScripts;
 	// Use this for initialization
 	void Awake(){
@@ -18,12 +21,29 @@ public class windowSetting : MonoBehaviour {
 		newWindow=GUI.Window(1,newWindow,WindowFunc,"Setting");
 	}
 	void WindowFunc(int windowID){
-		GUI.Label (new Rect (30, 20, 100, 30), "Time Limit");
-		Timer=float.Parse(GUI.TextField(new Rect(30,40,100,30),Timer.ToString()));
+		GUI.Label (new Rect (30, 20, 200, 30),"Time Limit "+Timer+"sec mode");
+		shortMode=GUI.Toggle(new Rect(30,40,50,30),shortMode,"30sec");
+		midleMode=GUI.Toggle(new Rect(90,40,50,30),midleMode,"60sec");
+		longMode=GUI.Toggle(new Rect(150,40,50,30),longMode,"90sec");
+		changeMode ();
 		GameControllerScripts.setTimer (Timer);
 		GUI.DragWindow ();
 	}
-	
+	void changeMode(){
+		if (shortMode&&Timer!=30) {
+			midleMode = false;
+			longMode = false;
+			Timer = 30f;
+		} else if (midleMode&&Timer!=60) {
+			shortMode = false;
+			longMode = false;
+			Timer = 60f;
+		} else if (longMode&&Timer!=90) {
+			shortMode=false;
+			midleMode=false;
+			Timer=90f;
+		}
+	}
 	// Update is called once per frame
 	void Update () {
 	
