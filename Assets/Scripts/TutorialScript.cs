5ea4ae4240df837f17d331c561da8db4789b339b
232fumiya <21311232fn@tama.ac.jp>
2016-03-15 19:27:47 +0900

    add tutorial

---
+++ Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialScript : MonoBehaviour {
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator> ();
+		anim.StopPlayback ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
